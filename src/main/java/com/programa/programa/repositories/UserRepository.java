f27b413a1c09b219f55d1a9c7233095389685eef
WeslleySilvaPinheiro <156719956+WeslleySilvaPinheiro@users.noreply.github.com>
2025-01-26 23:20:57 -0300

    Service layer, componetn registration

--- src/main/java/com/programa/programa/repositories/UserRepository.java
+++ src/main/java/com/programa/programa/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.programa.programa.entities.User;
 
+//optional notation 
 public interface UserRepository extends JpaRepository<User, Long>{
 		
 }
---
+++ src/main/java/com/programa/programa/services/UserServices.java
@@ -0,0 +1,26 @@
+package com.programa.programa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.programa.programa.entities.User;
+import com.programa.programa.repositories.UserRepository;
+
+@Service //component
+public class UserServices {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
